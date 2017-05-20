52246381d02a60c4c90937928bbfdc8ecf776fa1
Ian Robertson <falkensmaze@gmail.com>
2017-05-20 11:04:28 -0700

    Alter example Kafka producer to submit IP Datagrams

--- sensor/src/main/scala/edu/uw/at/iroberts/pcap/kafkaproducer/ProducerExample.scala
+++ sensor/src/main/scala/edu/uw/at/iroberts/pcap/kafkaproducer/ProducerExample.scala
@@ -1,8 +1,13 @@
 package edu.uw.at.iroberts.pcap.kafkaproducer
 
+import java.nio.ByteBuffer
 import java.util.Properties
 
+import akka.util.ByteString
+import edu.uw.at.iroberts.pcap.IPAddress
 import org.apache.kafka.clients.producer.{KafkaProducer, Producer, ProducerRecord}
+
+import scala.collection.immutable.Set.Set2
 
 /**
   * Created by scala on 5/12/17.
@@ -11,16 +16,24 @@ class ProducerExample {
   val props: Properties = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
   props.put("acks", "all")
-  props.put("retries", 0)
-  props.put("batch.size", 16384)
-  props.put("linger.ms", 1)
-  props.put("buffer.memory", 33554432)
+  props.put("retries", 0.asInstanceOf[java.lang.Integer])
+  props.put("batch.size", 16384.asInstanceOf[java.lang.Integer])
+  props.put("linger.ms", 1.asInstanceOf[java.lang.Integer])
+  props.put("buffer.memory", 33554432.asInstanceOf[java.lang.Integer])
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-  val producer: Producer[String, String] = new KafkaProducer(props)
+  type IPProto = Byte
+  type Port = Short
+  type ProducerKey = (IPProto, IPAddress, Port, IPAddress, Port)
+  type PacketProducerRecord = ProducerRecord[ProducerKey, ByteString]
+
+  val producer: Producer[ProducerKey, Array[Byte]] = new KafkaProducer(props)
   for (i <- 0 until 100) {
-    producer.send(new ProducerRecord[String, String]("my-topic", Integer.toString(i), Integer.toString(i)))
+    val key: ProducerKey = (4.toByte, IPAddress("192.168.0.1"), 25563.toShort, IPAddress("192.168.0.2"), 80.toShort)
+    val someByteString: ByteString = ???
+    val value: Array[Byte] = someByteString.toArray
+    producer.send(new ProducerRecord[ProducerKey, Array[Byte]]("ipv4-packets", key, value))
   }
 
   producer.close()
