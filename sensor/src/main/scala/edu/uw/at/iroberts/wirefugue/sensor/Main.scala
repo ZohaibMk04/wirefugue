9e65e4310079a219c87e7a5ab2395ae294014b2a
Ian Robertson <iroberts@uw.edu>
2017-05-24 16:11:10 -0700

    sensor.Main now publishes IP packets to Kafka (as either a byte array (commented out) or a string summary (verifiable with kafka-console-consumer)). TCP/UDP/ICMP flows with an identical set of endpoints (IP addr, port) end up in the same partition.

--- sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/sensor/Main.scala
+++ sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/sensor/Main.scala
@@ -3,11 +3,17 @@ package edu.uw.at.iroberts.wirefugue.sensor
 import java.nio.file.Paths
 
 import akka.actor.ActorSystem
+import akka.kafka.ProducerSettings
+import akka.kafka.scaladsl.Producer
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{FileIO, Sink}
+import com.typesafe.config.ConfigFactory
 import edu.uw.at.iroberts.pcap.PcapFileRaw.LinkType
 import edu.uw.at.iroberts.pcap._
+import edu.uw.at.iroberts.pcap.kafkaproducer.KafkaKey
 import edu.uw.at.iroberts.pcap.overlay.{Ethernet, IPV4Datagram}
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
 
 /**
   * Created by scala on 5/20/17.
@@ -19,8 +25,14 @@ object Main extends App {
       System.exit(1)
     }
 
-    implicit val system = ActorSystem()
+    val config = ConfigFactory.load("application.conf")
+
+    implicit val system = ActorSystem("stream-producer-system", config)
     implicit val materializer = ActorMaterializer()
+
+    //val producerSettings = ProducerSettings(system, KafkaKey.serializer, new ByteArraySerializer)
+    val producerSettings = ProducerSettings(system, KafkaKey.serializer, new StringSerializer)
+      .withBootstrapServers("localhost:9092,localhost:9093,localhost:9094")
 
     FileIO.fromPath(Paths.get(args(0)))
       .via(PcapFileParserRaw())
@@ -35,6 +47,9 @@ object Main extends App {
             IPV4Datagram(e.payload)
         }
       .alsoTo(Sink.foreach(println))
+      //.map( dg => new ProducerRecord[KafkaKey, Array[Byte]]("packets", KafkaKey.fromIPV4Datagram(dg), dg.bytes.toArray))
+      .map( dg => new ProducerRecord[KafkaKey, String]("packets", KafkaKey.fromIPV4Datagram(dg), dg.toString))
+      .alsoTo(Producer.plainSink(producerSettings))
       .runWith(Sink.onComplete { _ => system.terminate() })
   }
 }
