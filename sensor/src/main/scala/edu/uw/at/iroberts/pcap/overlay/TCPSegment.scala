ea79789f6238d460991d19e2d21af83c82935eff
Ian Robertson <iroberts@uw.edu>
2017-06-01 10:22:41 -0700

    Complete overlay for tcp segments

--- sensor/src/main/scala/edu/uw/at/iroberts/pcap/overlay/TCPSegment.scala
+++ sensor/src/main/scala/edu/uw/at/iroberts/pcap/overlay/TCPSegment.scala
@@ -6,7 +6,7 @@ import edu.uw.at.iroberts.pcap.ByteSeqOps._
 /**
   * Created by Ian Robertson <iroberts@uw.edu> on 5/22/17.
   */
-case class TCPSegment(bytes: IndexedSeq[Byte]) {
+object TCPSegment {
   object TcpFlagMask extends Enumeration {
     val FIN = Value(0x1)
     val SYN = Value(0x2)
@@ -18,17 +18,35 @@ case class TCPSegment(bytes: IndexedSeq[Byte]) {
     val CWR = Value(0x80)
     val NS = Value(0x100)
   }
-  import TcpFlagMask._
+}
+
+case class TCPSegment(bytes: IndexedSeq[Byte]) extends Overlay {
+
+  case class TCPFlags(asInt: Int) {
+    import TCPSegment.TcpFlagMask._
+
+    def fin: Boolean = (asInt & FIN.id) != 0
+    def syn: Boolean = (asInt & SYN.id) != 0
+    def rst: Boolean = (asInt & RST.id) != 0
+    def psh: Boolean = (asInt & PSH.id) != 0
+    def ack: Boolean = (asInt & ACK.id) != 0
+    def urg: Boolean = (asInt & URG.id) != 0
+    def ece: Boolean = (asInt & ECE.id) != 0
+    def cwr: Boolean = (asInt & CWR.id) != 0
+    def ns : Boolean = (asInt &  NS.id) != 0
+  }
+
   implicit val byteOrder = ByteOrder.BIG_ENDIAN
+
   def sport: Short = bytes.slice(0, 2).getInt16
   def dport: Short = bytes.slice(2, 4).getInt16
-  def seqN: Int = bytes.slice(4, 8).getInt32 // UNSIGNED
-  def ackN: Int = bytes.slice(8, 12).getInt32 // UNSIGNED
-  def flags: Int = bytes.slice(12, 14).getInt16 & 0x1f
-  def synFlag: Boolean = (flags & SYN.id) != 0
-  def finFlag: Boolean = (flags & FIN.id) != 0
-  def rstFlag: Boolean = (flags & RST.id) != 0
-  def ackFlag: Boolean = (flags & ACK.id) != 0
-  def pshFlag: Boolean = (flags & PSH.id) != 0
-  // TODO: add more fields
+  def sequenceNumber: Int = bytes.slice(4, 8).getInt32 // UNSIGNED
+  def acknowlegementNumber: Int = bytes.slice(8, 12).getInt32 // UNSIGNED
+  def dataOffset: Byte = (bytes(12) >>> 4).toByte
+  def flags: TCPFlags = TCPFlags(bytes.slice(12, 14).getInt16 & 0x01ff)
+  def windowSize: Short = bytes.slice(14, 16).getInt16 // UNSIGNED
+  def checksum: Short = bytes.slice(16, 18).getInt16 // UNSIGNED
+  def urgentPointer: Short = bytes.slice(18, 20).getInt16 // UNSIGNED
+  def options: IndexedSeq[Byte] = bytes.slice(20, dataOffset * 4)
+  def data: IndexedSeq[Byte] = bytes.drop(dataOffset * 4)
 }
