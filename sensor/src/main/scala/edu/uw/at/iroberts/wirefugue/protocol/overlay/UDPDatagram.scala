0576b485e211d833e9cf94bf2d0e6b1d7cb58053
Ian Robertson <iroberts@uw.edu>
2017-07-06 10:17:10 -0700

    Complete header for UDP

--- sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/protocol/overlay/UDPDatagram.scala
+++ sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/protocol/overlay/UDPDatagram.scala
@@ -1,12 +1,23 @@
 package edu.uw.at.iroberts.wirefugue.protocol.overlay
 
+import java.nio.ByteOrder
+
 import edu.uw.at.iroberts.wirefugue.pcap.ByteSeqOps._
 
 /**
   * Created by Ian Robertson <iroberts@uw.edu> on 5/22/17.
   */
-case class UDPDatagram(bytes: IndexedSeq[Byte]) {
-  def sport: Short = bytes.slice(0, 2).getInt16BE
-  def dport: Short = bytes.slice(2, 4).getInt16BE
-  // TODO: add more fields
+trait UDPHeader {
+  def sport: Short
+  def dport: Short
+  def length: Int
+  def checksum: Short
+}
+
+case class UDPDatagram(bytes: IndexedSeq[Byte]) extends UDPHeader {
+  implicit val byteOrder = ByteOrder.BIG_ENDIAN
+  def sport: Short = bytes.slice(0, 2).getInt16
+  def dport: Short = bytes.slice(2, 4).getInt16
+  def length: Int = bytes.slice(4, 6).getUInt16
+  def checksum: Short = bytes.slice(6, 8).getInt16
 }
