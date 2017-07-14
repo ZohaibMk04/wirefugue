5c8a1a5060a6114ce330f2ccdeae094234135704
Ian Robertson <iroberts@uw.edu>
2017-07-14 09:58:43 -0700

    Customize implicitNotFound for Timestamped

--- sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/Timestamped.scala
+++ sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/Timestamped.scala
@@ -2,11 +2,14 @@ package edu.uw.at.iroberts.wirefugue
 
 import java.time.Instant
 
+import scala.annotation.implicitNotFound
+
 /**
   * Created by Ian Robertson <iroberts@uw.edu> on 6/23/17.
   *
   * Type class for things that are temporally located
   */
+@implicitNotFound(msg = "No implicit Timestamped defined for ${E}.")
 trait Timestamped[E] {
   def timestamp(e: E): Instant
 }
