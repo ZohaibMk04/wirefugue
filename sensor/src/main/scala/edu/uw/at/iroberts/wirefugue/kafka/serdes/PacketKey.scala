e6cf43383ced6f72078aa5a4b6b90caaa097c764
Ian Robertson <iroberts@uw.edu>
2017-07-15 20:53:17 -0700

    Add custom hashing to PacketKey

--- sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/kafka/serdes/PacketKey.scala
+++ sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/kafka/serdes/PacketKey.scala
@@ -2,6 +2,8 @@ package edu.uw.at.iroberts.wirefugue.kafka.serdes
 
 import edu.uw.at.iroberts.wirefugue.pcap.IPAddress
 
+import scala.util.hashing.{Hashing, MurmurHash3}
+
 /**
   * Created by scala on 7/1/17.
   */
@@ -12,4 +14,35 @@ case class PacketKey(
                     sourcePort: Short,
                     destinationIP: IPAddress,
                     destinationPort: Short
-                    ) extends Serializable
+                    ) extends Serializable {
+  override def hashCode(): Int = PacketKey.packetKeyHashing.hash(this)
+
+}
+
+object PacketKey {
+  def packetKeyHashing = new Hashing[PacketKey] {
+    def hash(pk: PacketKey) = (pk.protocol ->
+      Set(
+        pk.sourceIP -> pk.sourcePort,
+        pk.destinationIP -> pk.destinationPort
+      )).##
+  }
+}
+
+object SimpleTest extends App {
+  val p1 = PacketKey(
+    3.toByte,
+    IPAddress("10.0.0.1"),
+    123.toShort,
+    IPAddress("192.168.0.1"),
+    456.toShort)
+  val p2 = PacketKey(
+    3.toByte,
+    IPAddress("192.168.0.1"),
+    456.toShort,
+    IPAddress("10.0.0.1"),
+    123.toShort,
+  )
+  println(p1.##)
+  println(p2.##)
+}
