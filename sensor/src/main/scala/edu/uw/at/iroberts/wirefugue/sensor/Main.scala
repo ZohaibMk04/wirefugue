988236e8e2c14e68d132b6c4596e2e434ae202ee
Ian Robertson <iroberts@uw.edu>
2017-07-17 09:55:20 -0700

    Update to sensor.Main
    - Simplification by use of application.conf configuration
    - Now uses Producer.flow instead of Producer.sink
    - Producer messages are printed after being produced
    - Better termination strategy
    - Doesn't override main in App, no longer extends App

--- sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/sensor/Main.scala
+++ sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/sensor/Main.scala
@@ -3,23 +3,28 @@ package edu.uw.at.iroberts.wirefugue.sensor
 import java.nio.file.Paths
 
 import akka.actor.ActorSystem
-import akka.kafka.ProducerSettings
+import akka.kafka.{ProducerMessage, ProducerSettings}
 import akka.kafka.scaladsl.Producer
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{FileIO, Sink}
 import com.typesafe.config.ConfigFactory
 import edu.uw.at.iroberts.wirefugue.pcap.PcapFileRaw.LinkType
 import edu.uw.at.iroberts.wirefugue.pcap._
-import edu.uw.at.iroberts.wirefugue.kafka.producer.KafkaKey
+import edu.uw.at.iroberts.wirefugue.kafka.producer.{KafkaKey, PacketProducer}
+import edu.uw.at.iroberts.wirefugue.kafka.serdes.PacketSerializer
 import edu.uw.at.iroberts.wirefugue.protocol.overlay.{Ethernet, IPV4Datagram}
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.ByteArraySerializer
+import org.apache.kafka.common.serialization.{ByteArraySerializer, IntegerSerializer}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 /**
-  * Created by scala on 5/20/17.
+  * Created by Ian Robertson <iroberts@uw.edu> on 5/20/17.
   */
-object Main extends App {
-  override def main(args: Array[String]) = {
+object Main {
+
+  def main(args: Array[String]): Unit = {
     if (args.length < 1) {
       println("Please specify a filename as the first argument")
       System.exit(1)
@@ -30,26 +35,20 @@ object Main extends App {
     implicit val system = ActorSystem("stream-producer-system", config)
     implicit val materializer = ActorMaterializer()
 
-    val producerSettings = ProducerSettings(system, KafkaKey.serializer, new ByteArraySerializer)
-    //val producerSettings = ProducerSettings(system, KafkaKey.serializer, new StringSerializer)
-      .withBootstrapServers("localhost:9092,localhost:9093,localhost:9094")
+    val producerSettings = ProducerSettings[Integer, Packet](system, None, None)
 
-    FileIO.fromPath(Paths.get(args(0)))
-      .via(PcapFileParserRawFlow())
-      // .alsoTo(Sink.foreach(println))
-      .via(PcapFileParser())
-        .collect {
-          case p if p.network == LinkType.ETHERNET =>
-            Ethernet(p.data)
-        }
-        .collect {
-          case e if e.etherType == EtherType.IPv4.id.toShort =>
-            IPV4Datagram(e.payload)
-        }
-      .alsoTo(Sink.foreach(println))
-      .map( dg => new ProducerRecord[KafkaKey, Array[Byte]]("packets", KafkaKey.fromIPV4Datagram(dg), dg.bytes.toArray))
-      //.map( dg => new ProducerRecord[KafkaKey, String]("packets", KafkaKey.fromIPV4Datagram(dg), dg.toString))
-      .alsoTo(Producer.plainSink(producerSettings))
-      .runWith(Sink.onComplete { _ => system.terminate() })
+    val doneF = PcapSource(Paths.get(args(0)).toUri)
+      .filter( p => p.network == LinkType.ETHERNET && p.ip.isDefined )
+      .map( packet => new ProducerRecord[Integer, Packet]("packets", packet.key.##, packet))
+      .map( pr => new ProducerMessage.Message[Integer, Packet, Unit](pr, ()))
+      .via(Producer.flow(producerSettings))
+      .runWith(Sink.foreach(println))
+
+    try {
+      Await.ready(doneF, 10 seconds)
+    }
+    finally {
+      system.terminate()
+    }
   }
 }
