b9d65f7bb088a95c1511121a649cff831fb8efcb
Ian Robertson <iroberts@uw.edu>
2017-07-16 17:39:00 -0700

    Update to PcapFileDemo app
    - Simplify PcapFileDemo to use PcapSource
    - Copied http.cap back to main/resources

--- sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/pcap/PcapFileDemo.scala
+++ sensor/src/main/scala/edu/uw/at/iroberts/wirefugue/pcap/PcapFileDemo.scala
@@ -8,17 +8,17 @@ import akka.stream.scaladsl._
 
 /** Prints out the packets in http.cap
   *
+  * Compare with the output of:
+  * $ TZ=UTC tcpdump -tttt -S -nn -r http.cap
+  *
   * Created by Ian Robertson <iroberts@uw.edu> on 4/3/17.
   */
 object PcapFileDemo extends App {
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
-  FileIO.fromPath(Paths.get("src/main/resources/bigFlows.pcap"))
-    .via(PcapFileParserRawFlow())
-    // .alsoTo(Sink.foreach(println))
-    .via(PcapFileParser())
-    .via(PacketLineFormatter())
+  val uri = getClass.getResource("/http.cap").toURI
+  PcapSource(uri)
     .alsoTo(Sink.foreach(println))
     .runWith(Sink.onComplete { _ => system.terminate() })
 
